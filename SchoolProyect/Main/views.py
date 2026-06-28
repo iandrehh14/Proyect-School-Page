4bcb95d6bd5433727105fe8d31de08af45cdddd1
iandrehh14 <iandrehh14@gmail.com>
2026-06-28 17:14:32 -0500

    Importacion inicial HomePage Designg y GeniallyPage

--- SchoolProyect/Main/views.py
+++ SchoolProyect/Main/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'Main/index.html')
 
+
+def schedule(request):
+    return render(request, 'main/schedule.html')
 # Create your views here.
